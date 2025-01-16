5be8ba07597350a0785605ca80819af457f0f621
Marc DeLay <marcdelay@gmail.com>
2025-01-16 11:42:52 -0600

    initial commit

---
+++ web/themes/marcdelaycom/js/marcdelaycom.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * marcdelaycom behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.marcdelaycom = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
